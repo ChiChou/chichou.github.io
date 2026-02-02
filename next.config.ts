737140e927607604ceae6afe8a542c83521a8829
cc <codecolorist@gmail.com>
2026-02-02 15:33:53 +0100

    add deploy action

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: basePath || undefined,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
