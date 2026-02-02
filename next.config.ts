2e64e36aed271baeb23cb6155b0e9ece9460fc4c
cc <codecolorist@gmail.com>
2026-02-02 15:45:03 +0100

    fix trailing slash

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   basePath: basePath || undefined,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
